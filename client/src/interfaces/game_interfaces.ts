0c3c41007f3b3e42bc0bd1f55e8790a26e6e8ec8
Armando Valencia <javm8@yahoo.com>
2025-05-15 21:05:52 -0500

    Build out initial client (#1)
    * WIP - building out client
    
    * Add basic game components to UI

---
+++ client/src/interfaces/game_interfaces.ts
@@ -0,0 +1,34 @@
+interface BaseGameState {
+    player_hand: string[];
+    dealer_hand: string[];
+    player_score: number;
+    dealer_score: number;
+    game_status: "waiting" | "player_turn" | "dealer_turn" | "game_over";
+    message: string;
+    result: "win" | "lose" | "push" | null;
+}
+
+export interface GameState extends BaseGameState {
+    type: "game_state";
+}
+
+export interface GameOverState extends BaseGameState {
+    type: "game_over";
+}
+
+// Define the ErrorMessage interface
+export interface ErrorMessage {
+    type: "error";
+    message: string;
+}
+
+export type ServerMessage = GameState | GameOverState | ErrorMessage;
+
+export interface ControlMessage {
+    type: "deal_initial" | "hit" | "stand";
+}
+
+export interface Card {
+  rank: string;
+  suit: string;
+}
