bf9d092b1071eccf5158fd429303b57d030246bc
Armando Valencia <javm8@yahoo.com>
2025-05-18 11:19:51 -0500

    Used flexbox to build card component

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,76 +1,59 @@
 import { useEffect, useState } from "react";
 
 interface CardProps {
-  card: string;
+	card: string;
 }
 
 interface CardValue {
-  rank: number | string;
-  suit: string;
+	rank: number | string;
+	suit: string;
 }
 
+const cardTemplate =
+	"max-w-sm min-w-28 p-3 m-4 rounded-md shadow-md min-h-40 flex flex-col border border-gray-200 font-bold";
+
 const Card: React.FC<CardProps> = ({ card }) => {
-  const [cardValue, setCardvalue] = useState<CardValue | null>(null);
+	const [cardValue, setCardvalue] = useState<CardValue | null>(null);
 
-  const suitMapper: { [key: string]: string } = {
-    H: "♥️",
-    D: "♦️",
-    C: "♣️",
-    S: "♠️",
-  };
+	const suitMapper: { [key: string]: string } = {
+		H: "♥️",
+		D: "♦️",
+		C: "♣️",
+		S: "♠️",
+	};
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const getCardValue = (card: string): CardValue => {
-    const rankString = card.substring(0, card.length - 1);
-    const parsedRank = parseInt(rankString);
-    const rank: string | number = isNaN(parsedRank) ? rankString : parsedRank;
-    const suit: string = suitMapper[card.charAt(card.length - 1)];
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const getCardValue = (card: string): CardValue => {
+		const rankString = card.substring(0, card.length - 1);
+		const parsedRank = parseInt(rankString);
+		const rank: string | number = isNaN(parsedRank)
+			? rankString
+			: parsedRank;
+		const suit: string = suitMapper[card.charAt(card.length - 1)];
 
-    return {
-      rank: rank,
-      suit: suit,
-    };
-  };
+		return {
+			rank: rank,
+			suit: suit,
+		};
+	};
 
-  useEffect(() => {
-    if (card !== "Hidden") setCardvalue(getCardValue(card));
-  }, [card, getCardValue]);
+	useEffect(() => {
+		if (card !== "Hidden") setCardvalue(getCardValue(card));
+	}, [card, getCardValue]);
 
-  if (card === "Hidden") {
-    return (
-      <div className="p-2 border rounded shadow text-center bg-gray-300 text-gray-500">
-        ?
-      </div>
-    );
-  }
-
-  //    card item with
-  return (
-    <>
-      {/* <div className={"p-2 border rounded shadow text-center bg-white"}>
-        <div className="flex row"></div>
-        {cardValue?.rank}
-        {cardValue?.suit}
-      </div> */}
-      <div className="grid grid-cols-5 grid-rows-5 gap-4 p-2 border rounded shadow text-center bg-white w-fit">
-        <div>{cardValue?.rank}</div>
-        <div></div>
-        <div></div>
-        <div className="row-start-2"></div>
-        <div className="row-start-2"></div>
-        <div className="row-start-2"></div>
-        <div className="row-start-3"></div>
-        <div className="row-start-3">{cardValue?.suit}</div>
-        <div className="row-start-3"></div>
-        <div className="row-start-4"></div>
-        <div className="row-start-4"></div>
-        <div className="row-start-4"></div>
-        <div className="row-start-5"></div>
-        <div className="row-start-5"></div>
-        <div className="row-start-5">{cardValue?.rank}</div>
-      </div>
-    </>
-  );
+	return card === "Hidden" ? (
+		<div className={`${cardTemplate} bg-gray-300`}>
+			<div className="flex justify-center items-center flex-grow">?</div>
+		</div>
+	) : (
+		<div className={`${cardTemplate} bg-white`}>
+			<div className="flex justify-start">{cardValue?.rank}</div>
+			<div className="flex justify-center items-center flex-grow">
+				{cardValue?.suit}
+			</div>
+			<div className="flex justify-end">{cardValue?.rank}</div>
+		</div>
+	);
 };
 
 export default Card;
