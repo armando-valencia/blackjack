1f5ddc94602eef2b453c544c2864b9ba82489090
Armando Valencia <javm8@yahoo.com>
2025-05-18 11:25:02 -0500

    Add styling to card component (#2)
    * Clean up card component
    
    * Used flexbox to build card component
    
    * Formatting

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,21 +1,55 @@
+import { useEffect, useState } from "react";
+
 interface CardProps {
-  card: string;
+	card: string;
 }
 
-const Card: React.FC<CardProps> = ({ card }) => {
-  if (card === "Hidden") {
-    return (
-      <div className="p-2 border rounded shadow text-center bg-gray-300 text-gray-500">
-        ?
-      </div>
-    );
-  }
+interface CardValue {
+	rank: number | string;
+	suit: string;
+}
 
-  return (
-    <div className={"p-2 border rounded shadow text-center bg-white"}>
-      {card} {/* Display the card string */}
-    </div>
-  );
+const cardTemplate =
+	"max-w-sm min-w-28 p-3 m-4 rounded-md shadow-md min-h-40 flex flex-col border border-gray-200 font-bold";
+
+const Card: React.FC<CardProps> = ({ card }) => {
+	const [cardValue, setCardvalue] = useState<CardValue | null>(null);
+
+	const suitMapper: { [key: string]: string } = {
+		H: "♥️",
+		D: "♦️",
+		C: "♣️",
+		S: "♠️",
+	};
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const getCardValue = (card: string): CardValue => {
+		const rankString = card.substring(0, card.length - 1);
+		const parsedRank = parseInt(rankString);
+		const rank: string | number = isNaN(parsedRank) ? rankString : parsedRank;
+		const suit: string = suitMapper[card.charAt(card.length - 1)];
+
+		return {
+			rank: rank,
+			suit: suit,
+		};
+	};
+
+	useEffect(() => {
+		if (card !== "Hidden") setCardvalue(getCardValue(card));
+	}, [card, getCardValue]);
+
+	return card === "Hidden" ? (
+		<div className={`${cardTemplate} bg-gray-300`}>
+			<div className="flex justify-center items-center flex-grow">?</div>
+		</div>
+	) : (
+		<div className={`${cardTemplate} bg-white`}>
+			<div className="flex justify-start">{cardValue?.rank}</div>
+			<div className="flex justify-center items-center flex-grow">{cardValue?.suit}</div>
+			<div className="flex justify-end">{cardValue?.rank}</div>
+		</div>
+	);
 };
 
 export default Card;
